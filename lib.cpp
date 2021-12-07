5878504f3559c38e7714d806b2fa1d9ee04db0a1
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-07 13:49:10 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include "lib.h"
 using namespace std;
-double vett( ) {
 
-    int i,n, a[n], max, p;
+
+double vett( ) {
+    int i,n;
+    double   max;
     cin>>n;
+    double a[n];
     for (i = 0; i < n; i++) {
         cin >> a[i];
     }
     max = a[0];
-   
+
     for (i = 0; i < n; i++) {
         if (a[i] > max) {
             max = a[i];
-           
+
         }
     }
     return max;
 }
-
